445dc944dff66cae1d67c9a70eba75f7aecb83d5
Gopal <Gopal1409@gmail.com>
2021-08-24 04:43:58 -0700

    terraform workspace

--- prod/c5-output.tf
+++ prod/c5-output.tf
@@ -2,6 +2,6 @@ output "ec2_instance_public_ip" {
   value = aws_instance.my-ec2-vm.*.public_ip
 }
 
-output "ec2_instance_public_ip" {
+output "ec2_instance_public_dns" {
   value = aws_instance.my-ec2-vm.*.public_dns
 }
