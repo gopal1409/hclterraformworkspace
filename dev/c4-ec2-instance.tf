4459152b758bf5124f5d6143cb99ccd08202073f
Gopal <Gopal1409@gmail.com>
2021-08-24 05:41:46 -0700

    terraform workspace

--- dev/c4-ec2-instance.tf
+++ dev/c4-ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-vm" {
   #instance_type = var.instance_type
   #instance_type = var.instance_type_list[1] #for list
   instance_type = var.instance_type
-  count = terraform.workspace == "prod" ? 2:1
+  count = terraform.workspace == "default" ? 2:1
   user_data = file("apache-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
   
